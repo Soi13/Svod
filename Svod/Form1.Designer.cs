c6dc8576eb864714d2855f815e4271564e5a0ccd
Oleg Skvortsov <highsoft.soi@gmail.com>
2019-10-18 12:20:30 -0700

    Svod

---
+++ Svod/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Svod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать БДДС подразделения";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выбрать сводный БДДС";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Файл:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Файл Excel|*.XLSX;*.XLS;*.XLSM;*.XLSB";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(134, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(400, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Формирование сводного БДДС";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(15, 220);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "С Т А Р Т";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Файл:";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.Filter = "Файл Excel|*.XLSX;*.XLS;*.XLSM;*.XLSB";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Обработано строк:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 305);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(625, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(286, 289);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Заполнение:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 332);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Формирование свода";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Svod/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+
+namespace Svod
+{
+    public partial class Form1 : Form
+    {
+        private Microsoft.Office.Interop.Excel.Application ObjExcel;
+        private Microsoft.Office.Interop.Excel.Workbook ObjWorkBook;
+        private Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = openFileDialog1.FileName;
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            /////Создание объекта БДДС подразделения
+            Microsoft.Office.Interop.Excel.Application ObjExcel = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook ObjWorkBook = ObjExcel.Workbooks.Open(openFileDialog1.FileName, 0, false, 5, "", "", false, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+            Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
+            ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Sheets["ПЛАН В"];
+            /////////
+
+            /////Создание объекта сводный БДДС
+            Microsoft.Office.Interop.Excel.Application ObjExcel1 = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook ObjWorkBook1 = ObjExcel1.Workbooks.Open(openFileDialog2.FileName, 0, false, 5, "", "", false, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+            Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet1;
+            ObjWorkSheet1 = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook1.Sheets["ПЛАН В"];
+            /////////
+
+            //////////////////Заполнение ПЛАН В/////////////////////////////
+            int nn = 1;
+            progressBar1.Maximum = 505;
+            label5.Text = "Заполнение ПЛАН В";
+
+            //Цикл по БДДС подразделения  
+            for (int s = 4; s<=505; s++)
+            {
+                double stolbec_summa_bdds_podr = Convert.ToDouble(ObjWorkSheet.Cells[s, 5].value);
+
+                //Определение цвета. Если не желтый, то пропускаем строку
+                int color = int.Parse(ObjWorkSheet.Cells[s, 5].Interior.Color.ToString());
+                if (color != 65535) { continue; }
+
+                if (stolbec_summa_bdds_podr > 0)
+                {
+                    double stolbec_summa_svod = Convert.ToDouble(ObjWorkSheet1.Cells[s, 5].value);
+                    double sum_common = stolbec_summa_svod + stolbec_summa_bdds_podr;
+                    ObjWorkSheet1.Cells[s, 5] = sum_common;
+
+                }
+                
+                for (int d = 15; d <= 45; d++)
+                 {
+                     //string date_bdds_podr = ObjWorkSheet.Cells[3, d].Text;
+                     double summa_bdds_podr = Convert.ToDouble(ObjWorkSheet.Cells[s, d].value);
+
+                     if (summa_bdds_podr>0)
+                     {
+                         double st = Convert.ToDouble(ObjWorkSheet1.Cells[s, d].value);
+                         double sum = st + summa_bdds_podr;
+                         ObjWorkSheet1.Cells[s, d] = sum;
+                     }
+         
+                 }
+                nn = nn + 1;
+                progressBar1.Value = nn;
+                label4.Text ="Обработано строк: " + Convert.ToString(nn);
+            }
+            ///////////////////////////////////////////////////////////////////
+
+
+            
+            
+            Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet2;
+            ObjWorkSheet2 = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Sheets["ПЛАН П"];
+            /////////
+            Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet3;
+            ObjWorkSheet3 = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook1.Sheets["ПЛАН П"];
+            /////////
+
+            //////////////////Заполнение ПЛАН П/////////////////////////////
+            int nn1 = 1;
+            progressBar1.Maximum = 505;
+            label5.Text = "Заполнение ПЛАН П";
+
+            //Цикл по БДДС подразделения  
+            for (int s1 = 4; s1 <= 505; s1++)
+            {
+                if (s1 == 4 || s1 == 5 || s1 == 6 || s1 == 26 || s1 == 31 || s1 == 37 || s1 == 53 || s1 == 55 || s1 == 59 || s1 == 62 || s1 == 64 || s1 == 75 || s1 == 89 || s1 == 100 || s1 == 136 || s1 == 146 || s1 == 153 || s1 == 156 || s1 == 164 || s1 == 168 || s1 == 175 || s1 == 184 || s1 == 185 || s1 == 195 || s1 == 196 || s1 == 205 || s1 == 209 || s1 == 210 || s1 == 222) continue;
+                
+                double stolbec_summa_bdds_podr1 = Convert.ToDouble(ObjWorkSheet2.Cells[s1, 5].value);
+                if (stolbec_summa_bdds_podr1 > 0)
+                {
+                    double stolbec_summa_svod1 = Convert.ToDouble(ObjWorkSheet3.Cells[s1, 5].value);
+                    double sum_common1 = stolbec_summa_svod1 + stolbec_summa_bdds_podr1;
+                    ObjWorkSheet3.Cells[s1, 5] = sum_common1;
+
+                }
+                         
+                for (int d1 = 15; d1 <= 45; d1++)
+                {
+                    //string date_bdds_podr = ObjWorkSheet.Cells[3, d].Text;
+                    double summa_bdds_podr1 = Convert.ToDouble(ObjWorkSheet2.Cells[s1, d1].value);
+
+                    if (summa_bdds_podr1 > 0)
+                    {
+                        double st1 = Convert.ToDouble(ObjWorkSheet3.Cells[s1, d1].value);
+                        double sum = st1 + summa_bdds_podr1;
+                        ObjWorkSheet3.Cells[s1, d1] = sum;
+                    }
+
+                }
+                nn1 = nn1 + 1;
+                progressBar1.Value = nn1;
+                label4.Text = "Обработано строк: " + Convert.ToString(nn1);
+            }
+            ///////////////////////////////////////////////////////////////////
+            
+            
+            ObjWorkBook.Close();
+            ObjExcel.Quit();
+            ObjWorkBook = null;
+            ObjWorkSheet = null;
+            ObjExcel = null;
+
+            ObjExcel1.Visible = true;
+            
+            GC.Collect(); 
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {                
+                    label3.Text = openFileDialog2.FileName;
+            }
+        }
+    }
+}
